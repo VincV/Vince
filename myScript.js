17013da6d64e846f78bc087f4494fb8e72095713
VincV <57570866+VincV@users.noreply.github.com>
2019-11-09 19:04:35 +0100

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,25 @@
+function csgo() {
+  document.getElementById("info").innerHTML= "<h2>THHHHHHIMING<br><br>And other ridiculous excuses </h2>";
+  document.getElementById("players").innerHTML= "<br><br>Players : <br>Mr.useless - Drawing <br>Nizarthi - rushing <br>Pomas - Forced to be playing <br>Topvennie - Raging <br>Wiki - Scouting";
+  document.getElementById("minecr").innerHTML= "";
+}
+function rocketLeague() {
+  document.getElementById("info").innerHTML= "<h2>Haters call it car soccer</h2>";
+  document.getElementById("players").innerHTML= "<br><br>Drivers: <br>Pomas <br>Topvennie";
+  document.getElementById("minecr").innerHTML= "";
+}
+function overwatch() {
+  document.getElementById("info").innerHTML= "<h2>Sorry, we support Hongkong!</h2>";
+  document.getElementById("players").innerHTML="";
+  document.getElementById("minecr").innerHTML= "";
+}
+function minecraft() {
+  document.getElementById("info").innerHTML= "<h2>What did the squid say?<br><br>RING-DING-DING-DINGERINGEDING! </h2>";
+  document.getElementById("players").innerHTML= "<br><br>Server members: <br>Blobvis aka Mr.useless<br>Nizarthi<br>Pomas<br>Topvennie<br>Wiki";
+  document.getElementById("minecr").innerHTML= "Server map";
+}
+function fortnite() {
+  document.getElementById("info").innerHTML="<h2>Shit game LOL</h2>";
+  document.getElementById("players").innerHTML="<br><br>No thanks";
+  document.getElementById("minecr").innerHTML= "";
+}
